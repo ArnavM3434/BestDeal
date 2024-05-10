a8c58647fcac7c48f83938e1fdd6ce1327deae3f
ArnavM3434 <arnavnmehta1@gmail.com>
2024-05-10 10:17:08 -0500

    s

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -8,20 +8,12 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
+            "https://www.google.com/search?q=red+car"
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        # filename = f"quotes-{page}.html"
-        # Path(filename).write_bytes(response.body)
-        # self.log(f"Saved file {filename}")
-        for quote in response.css("div.quote"):
-            yield{
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+        Path("redcartest.html").write_bytes(response.body)
+
+       
